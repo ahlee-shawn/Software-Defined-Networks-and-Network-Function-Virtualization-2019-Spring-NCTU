9471e5c90faab86f0d0ede40da71be566db80456
leeang6969 <leeang6969.cs05@g2.nctu.edu.tw>
2019-03-07 22:29:10 +0800

    Finish Lab1

--- Lab1/project1_0516327.py
+++ Lab1/project1_0516327.py
@@ -1,4 +1,5 @@
 from mininet.topo import Topo
+from mininet.cli import CLI
 
 class Project_Topo_0516327(Topo):
 	def __init__(self):
@@ -27,3 +28,23 @@ class Project_Topo_0516327(Topo):
 		self.addLink(s2, s4)
 
 topos = {"topology_0516327": Project_Topo_0516327}
+
+
+def addHostToSwitch( self, line ):
+	"addHostToSwitch is a command to add a host to a given switch"
+	net = self.mn
+
+	host_name = "h" + str(len(net.hosts) + 1)
+	h_new = net.addHost(host_name)
+
+	target_switch = net.getNodeByName(line)
+
+	link = net.addLink(h_new, line)
+
+	interface = str(link).split('<->')[1]
+	target_switch.attach(interface)
+
+	IP = "10.0.0." + str(len(net.hosts))
+	h_new.setIP(IP)
+
+CLI.do_addHostToSwitch = addHostToSwitch
